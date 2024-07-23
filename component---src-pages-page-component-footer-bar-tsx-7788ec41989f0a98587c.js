da4b36377595ac332f7e17da4b27b5c9f58ffa85
watchmen-r <watchmen0205@yahoo.co.jp>
2024-07-23 14:08:34 +0900

    Updates

---
+++ component---src-pages-page-component-footer-bar-tsx-7788ec41989f0a98587c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[422],{3510:function(e,t,a){a.r(t);var o=a(7294),n=a(3906),r=a(2658);t.default=()=>o.createElement(n.Z,{component:"footer",sx:{display:"flex",justifyContent:"center",alignItems:"center",padding:2,backgroundColor:"white",color:"black",position:"fixed",bottom:0,width:"100%"}},o.createElement(r.Z,{variant:"body1"},"© ",(new Date).getFullYear()," createdby oda"))}}]);
+//# sourceMappingURL=component---src-pages-page-component-footer-bar-tsx-7788ec41989f0a98587c.js.map
